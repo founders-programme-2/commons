116ec6f612f41c090acbd51cfc89007df9f51f45
ali <ali.7@outlook.com>
2019-04-14 17:52:55 +0300

    test-eslint-config-works relate #10

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {};
+
   render() {
-    return (
-      <h1>Commons</h1>
-    );
+    return <h1>Commons</h1>;
   }
 }
 
