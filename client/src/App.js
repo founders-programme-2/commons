bbf758a9137d7a6e0697245c6ce8a6805f13f886
Kristina Jaggard <jaggardk@gmail.com>
2019-04-17 13:22:44 +0100

    create pages for all of menu items, route components in app.js and link routes in burger menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-import Home from './component/views/Home';
+// burger menu components
 import Menu from './component/common/Menu';
+import Home from './component/views/Home';
+import About from './component/views/About';
+import HowTo from './component/views/HowTo';
+import Play from './component/views/Play';
+import Methods from './component/views/Methods';
+import KeyTerms from './component/views/KeyTerms';
+import Workshop from './component/views/Workshop';
+import Theory from './component/views/Theory';
 
 class App extends Component {
   state = {};
@@ -14,6 +22,13 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/menu" component={Menu} />
+          <Route path="/about" component={About} />
+          <Route path="/howTo" component={HowTo} />
+          <Route path="/play" component={Play} />
+          <Route path="/methods" component={Methods} />
+          <Route path="/keyTerms" component={KeyTerms} />
+          <Route path="/workshop" component={Workshop} />
+          <Route path="/theory" component={Theory} />
         </Switch>
       </BrowserRouter>
     );
