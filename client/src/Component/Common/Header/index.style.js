3414237277e21a546c4671aaf677eb41b62347f3
ali <ali.7@outlook.com>
2019-04-17 16:39:55 +0300

    add-styled-component-Header relate #28

---
+++ client/src/Component/Common/Header/index.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 4rem 2.5rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 15rem;
+  text-align: center;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+`;
+
+export const Img = styled.img`
+  width: 8.5rem;
+  height: 8.5rem;
+  display: block;
+`;
+
+export const Line = styled.div`
+  display: block;
+  border: 0.5rem solid #faa634;
+  border-radius: 1rem;
+`;
