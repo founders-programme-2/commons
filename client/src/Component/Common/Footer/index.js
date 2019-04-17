7dea087859b766e28d536ea52c5c8045160dbff0
hshahwan <haneen.shahwan9@gmail.com>
2019-04-17 10:37:11 +0300

    rename folders and files as camelCase style

---
+++ client/src/Component/Common/Footer/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  state = {};
+
+  render() {
+    return (
+        <React.Fragment>
+        <button></button>
+        <button></button>
+        </React.Fragment>
+    )
+  }
+}
+
+export default Footer;
