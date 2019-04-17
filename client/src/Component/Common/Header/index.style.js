ff176227b5edf4fa88864d3e2c2ef3ee70ed8b43
ali <ali.7@outlook.com>
2019-04-17 17:53:22 +0300

    add-style-to-HeaderTitle relate #28

--- client/src/Component/Common/Header/index.style.js
+++ client/src/Component/Common/Header/index.style.js
@@ -6,14 +6,7 @@ export const HeaderWrapper = styled.div`
   margin: 4rem 2.5rem;
 `;
 
-export const Title = styled.h1`
-  font-size: 15rem;
-  text-align: center;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-`;
-
-export const Img = styled.img`
+export const Logo = styled.img`
   width: 8.5rem;
   height: 8.5rem;
   display: block;
@@ -21,6 +14,24 @@ export const Img = styled.img`
 
 export const Line = styled.div`
   display: block;
-  border: 0.5rem solid #faa634;
+  border: 0.3rem solid #faa634;
   border-radius: 1rem;
+`;
+
+export const HeaderImg = styled.img`
+  width: 17rem;
+  height: 17rem;
+  margin-right: 2rem;
+`;
+
+export const HeaderTitle = styled.div`
+  display: flex;
+  margin: 2rem 2rem;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 5rem;
+  font-weight: 900;
+  color: #000000;
 `;
