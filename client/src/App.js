9ecd8c8021a12797eb8c7208da121e084369e460
Kristina Jaggard <jaggardk@gmail.com>
2019-04-17 10:24:25 +0100

    basic set-up for react-router-dom with routes for home and burgermenu

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
+
+import Home from './component/views/Home';
+import BurgerMenu from './component/common/BurgerMenu';
 
 class App extends Component {
   state = {};
 
   render() {
-    return <h1>Commons</h1>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/menu" component={BurgerMenu} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
