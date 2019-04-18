68aa0d44230399a0758d0bbe040e348ec6960538
hshahwan <haneen.shahwan9@gmail.com>
2019-04-18 15:48:02 +0300

    method views with header and footer;

--- client/src/Component/views/Methods/index.js
+++ client/src/Component/views/Methods/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../../Common/Header';
+import Footer from '../../Common/Footer';
 
 const Methods = () => {
   return (
     <div>
+      <Header titleText="Selecet your methods" />
       <h1>Hello from Methods.js</h1>
+      <Footer />
     </div>
   );
 };
