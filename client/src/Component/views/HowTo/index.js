c204a6f837c97325f60bef571af918510a482ec3
hshahwan <haneen.shahwan9@gmail.com>
2019-04-18 14:34:32 +0300

    create how to layout and prop the tilte and icon for the view

---
+++ client/src/Component/views/HowTo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../Common/Header';
+import instruction from '../../../assets/instruction.svg';
+
+const HowTo = () => {
+  return (
+    <div>
+      <Header headerImg={instruction} titleText="How to play" />
+      <h1>How to play</h1>
+    </div>
+  );
+};
+
+export default HowTo;
