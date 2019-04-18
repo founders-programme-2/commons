105123d263e4fc904b43870f236c4678a9fe9235
hshahwan <haneen.shahwan9@gmail.com>
2019-04-18 15:16:25 +0300

    passing header and footer for how to play page

--- client/src/Component/views/HowTo/index.js
+++ client/src/Component/views/HowTo/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from '../../Common/Header';
+import Footer from '../../Common/Footer';
 import instruction from '../../../assets/instruction.svg';
 
 const HowTo = () => {
   return (
     <div>
       <Header headerImg={instruction} titleText="How to play" />
-      <h1>How to play</h1>
+      <h3>How to play</h3>
+      <h3>How to play</h3>
+      <h3>How to play</h3>
+      <h3>How to play</h3>
+      <h3>How to play</h3>
+      <Footer />
     </div>
   );
 };
