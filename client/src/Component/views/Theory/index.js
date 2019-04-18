f4203423d46c2d3a14e2051554478fd9e9fc76f4
hshahwan <haneen.shahwan9@gmail.com>
2019-04-18 16:06:01 +0300

    theory views ready to build

--- client/src/Component/views/Theory/index.js
+++ client/src/Component/views/Theory/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../../Common/Header';
+import Footer from '../../Common/Footer';
 
 const Theory = () => {
   return (
     <div>
+      <Header />
       <h1>Hello from Theory.js</h1>
+      <Footer />
     </div>
   );
 };
