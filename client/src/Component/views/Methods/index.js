5f5e4f28b3d280df922b4c143801486a2d65bef6
hshahwan <haneen.shahwan9@gmail.com>
2019-04-19 18:05:47 +0300

    passing props with null value for image in header component

--- client/src/Component/views/Methods/index.js
+++ client/src/Component/views/Methods/index.js
@@ -5,7 +5,7 @@ import Footer from '../../Common/Footer';
 const Methods = () => {
   return (
     <div>
-      <Header titleText="Selecet your methods" />
+      <Header headerImg={null} titleText="Select your methods" />
       <h1>Hello from Methods.js</h1>
       <Footer />
     </div>
