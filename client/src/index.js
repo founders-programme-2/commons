7a02ed71c7205f603e57aff5bc493588def03c49
ali <ali.7@outlook.com>
2019-04-21 01:57:07 +0300

    remove-BrowserRouter-from-wrapping-App relate #52

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import App from './App';
+import './index.css';
 
 library.add(faBars, faTimes);
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
