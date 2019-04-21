36cc299c38accc55a096f8e14ca8bf2bcd0a7c7d
Kristina Jaggard <jaggardk@gmail.com>
2019-04-21 12:07:40 +0100

    creation and basic card styling

--- client/src/Component/views/MethodCard/index.js
+++ client/src/Component/views/MethodCard/index.js
@@ -1,1 +1,32 @@
-import React from 'react';
+import { React, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { CardWrapper, Img, Info, MoreInfo } from './index.style';
+
+const CardComponent = props => {
+  const { cardImg, cardTitle, resourcePoints } = props;
+
+  return (
+    <Fragment>
+      <CardWrapper>
+        <Img>{cardImg}</Img>
+        <Info>
+          {cardTitle}
+          <MoreInfo>Flip for more info...</MoreInfo>
+          {resourcePoints}
+        </Info>
+      </CardWrapper>
+    </Fragment>
+  );
+};
+
+CardComponent.propTypes = {
+  cardImg: PropTypes.string,
+  cardTitle: PropTypes.string.isRequired,
+  resourcePoints: PropTypes.number.isRequired,
+};
+
+CardComponent.defaultProps = {
+  cardImg: PropTypes.bool,
+};
+
+export default CardComponent;
--- client/src/Component/views/MethodCard/index.style.js
+++ client/src/Component/views/MethodCard/index.style.js
@@ -1,1 +1,30 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+
+export const CardWrapper = styled.section`
+  background: var(--background-color);
+  border-radius: 5px 5px 5px 5px;
+  height: 27rem;
+  width: 22rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Img = styled.img`
+  border-radius: 5px 5px 0px 0px;
+  background: var(--highlight-color);
+  width: 22rem;
+  height: 13.5rem;
+`;
+
+export const Info = styled.section`
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 0px 5px 5px;
+  width: 22rem;
+  height: 13.5rem;
+`;
+
+export const MoreInfo = styled(Link)`
+  font-family: Poppins;
+`
