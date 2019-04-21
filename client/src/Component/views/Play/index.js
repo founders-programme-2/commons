ac154e69fa31f5e6cac9f656f0444d27b691a8ac
hshahwan <haneen.shahwan9@gmail.com>
2019-04-21 14:11:43 +0300

    create two button for play random scenario and own context

--- client/src/Component/views/Play/index.js
+++ client/src/Component/views/Play/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
+import { Button, ButtonWrapper } from './index.style';
 
 const Play = () => {
+  
   return (
     <div>
       <Header headerImg={null} titleText="Choose your game play" />
-      <h1>Choose your game play:</h1>
+      <ButtonWrapper>
+        <Button>play a random scenario</Button>
+        <br />
+        <Button>play with your own context</Button>
+      </ButtonWrapper>
       <Footer />
     </div>
   );
