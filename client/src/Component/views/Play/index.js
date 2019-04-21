629dc7f54c07c217a43bb0b1510edaa3da188e43
hshahwan <haneen.shahwan9@gmail.com>
2019-04-21 15:21:39 +0300

    link buttons with home page until other layouts be ready

--- client/src/Component/views/Play/index.js
+++ client/src/Component/views/Play/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
 import { Button, ButtonWrapper } from './index.style';
 
-const Play = () => {
-  
-  return (
-    <div>
-      <Header headerImg={null} titleText="Choose your game play" />
-      <ButtonWrapper>
-        <Button>play a random scenario</Button>
-        <br />
-        <Button>play with your own context</Button>
-      </ButtonWrapper>
-      <Footer />
-    </div>
-  );
-};
+class Play extends Component {
+  state = {};
+
+  playRandomScenario = () => {
+    this.props.history.push('/');
+  };
+
+  ownContext = () => {
+    this.props.history.push('/');
+  }
+
+  render() {
+    return (
+      <div>
+        <Header headerImg={null} titleText="Choose your game play" />
+        <ButtonWrapper>
+          <Button onClick={this.playRandomScenario}>
+            play a random scenario
+          </Button>
+          <br />
+          <Button onClick={this.ownContext}>play with your own context</Button>
+        </ButtonWrapper>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Play;
