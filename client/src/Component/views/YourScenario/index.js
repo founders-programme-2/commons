f59da1319969246772f0a16d2313ed4249c65f99
hshahwan <haneen.shahwan9@gmail.com>
2019-04-22 15:23:14 +0300

    adding content in the page

--- client/src/Component/views/YourScenario/index.js
+++ client/src/Component/views/YourScenario/index.js
@@ -7,7 +7,29 @@ const YourScenario = () => {
   return (
     <div>
       <Header headerImg={houses} titleText="Your scenario" />
-      <Footer />
+      <div>
+        <p>
+          The Slipham social action centre is launching a Connected Commons
+          programme, aiming to help people explore its heritage, find local
+          opportunities and services, and support new projects for a better
+          Slipham in the future. It will do this by mapping people, projects and
+          groups to find what they can offer, what they need, and how they are
+          connected at present. The Commons won&apos;t be just one physical
+          space. It will be on-the-ground trails and gathering points, maps of
+          doing what, a website and other communication tools, with guides for
+          action. We&apos;ll have small grants to support projects, and help on
+          fundraising. The overall aim is to create an environment within which
+          connections and collaborations can take place - the Slipham Connected
+          Commons.
+        </p>
+        <br />
+        <p style={{ fontWeight: 'bold' }}>
+          Your goal is to select the tools that you would use to faciliate
+          creating this Connected Commons Program. You have a limited amount of
+          resources and must choose wisely.
+        </p>
+      </div>
+      <Footer backLink="/play" nextLink="/" reviewScenario={false} />
     </div>
   );
 };
