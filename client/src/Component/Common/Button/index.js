5828948965305c932fcc88bec5d58e91da5d8cba
hshahwan <haneen.shahwan9@gmail.com>
2019-04-22 12:40:56 +0300

    passing widht in props and create validation for width

--- client/src/Component/Common/Button/index.js
+++ client/src/Component/Common/Button/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { PrimaryButton } from './index.style';
 
 const Button = props => {
-  const { buttonText, onClick, height } = props;
+  const { buttonText, onClick, height, width } = props;
 
   return (
-    <PrimaryButton onClick={onClick} height={height}>
+    <PrimaryButton onClick={onClick} height={height} width={width}>
       {buttonText}
     </PrimaryButton>
   );
@@ -16,9 +16,11 @@ Button.propTypes = {
   buttonText: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   height: PropTypes.string,
+  width: PropTypes.string,
 };
 Button.defaultProps = {
   height: PropTypes.checkPropTypes(),
+  width: PropTypes.checkPropTypes(),
 };
 
 export default Button;
