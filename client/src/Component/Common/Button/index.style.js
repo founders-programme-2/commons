99469902adbdd4014c4e071b5c3ff155391cebad
hshahwan <haneen.shahwan9@gmail.com>
2019-04-22 14:39:28 +0300

    addign padding and font-weight for button

--- client/src/Component/Common/Button/index.style.js
+++ client/src/Component/Common/Button/index.style.js
@@ -7,12 +7,12 @@ export const PrimaryButton = styled.button`
   width: ${props => props.width || '12.5rem'};
   border: none;
   font-size: 1.5rem;
-  font-weight: 1rem;
   border-radius: 0.5rem;
   color: black;
-  font-weight: bold;
+  font-weight: 800;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   margin: 1rem;
+  padding: 1rem;
 
   :hover {
     cursor: pointer;
