f20e5338ab4c63ba09192167fe1526ccc0b79a08
ali <ali.7@outlook.com>
2019-04-22 13:54:03 +0300

    add-styling-to-Howto-page-body relate #17

---
+++ client/src/Component/views/HowTo/inedx.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const BodyWrapper = styled.div`
+  height: 100%;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  display: flex;
+`;
+
+export const PulpIcon = styled.div`
+  color: var(--arrow-color);
+  padding: 1rem;
+`;
+
+export const Text = styled.p`
+  padding: 1rem;
+  font-weight: 700;
+`;
