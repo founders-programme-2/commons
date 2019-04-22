63b3a6351122aaf291a129768387e8c875bfc333
hshahwan <haneen.shahwan9@gmail.com>
2019-04-22 11:14:06 +0300

    link button with home page

--- client/src/Component/views/Play/index.js
+++ client/src/Component/views/Play/index.js
@@ -3,14 +3,21 @@ import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
 import Button from '../../Common/Button';
 
-
-const Play = () => {
+const Play = props => {
+  // eslint-disable-next-line react/prop-types
+  const { history } = props;
   return (
     <div>
       <Header headerImg={null} titleText="Choose your game play" />
       <div>
-        <Button />
-        <Button />
+        <Button
+          buttonText="Play a random scenario"
+          onClick={() => history.push('/')}
+        />
+        <Button
+          buttonText="Play with your own context"
+          onClick={() => history.push('/')}
+        />
       </div>
       <Footer />
     </div>
