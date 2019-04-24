67ef7beb7ea746657a146a73c03667d13766289d
hshahwan <haneen.shahwan9@gmail.com>
2019-04-24 13:16:53 +0300

    adding choose button for persona page

--- client/src/Component/views/Persona/index.js
+++ client/src/Component/views/Persona/index.js
@@ -4,9 +4,10 @@ import Footer from '../../Common/Footer';
 import { Description } from './index.style';
 import { CardWrapper, Img, CardTitle, Info } from '../MethodCard/index.style';
 import logo from '../../../assets/logo.svg';
+import Button from '../../Common/Button';
 
 const Persona = props => {
-  const { heightCard, heightImg, textAlign, heightInfoCard,widthtCard } = props;
+  const { heightCard, heightImg, textAlign, heightInfoCard,widthtCard, buttonText } = props;
   return (
     <div>
       <Header headerImg={null} titleText="Selection your persona" />
@@ -21,6 +22,7 @@ const Persona = props => {
           </Description>
         </Info>
       </CardWrapper>
+      <Button buttonText="Choose" />
       <Footer backLink="/yourScenario" nextLink="/" reviewScenario={false} />
     </div>
   );
--- client/src/Component/views/Persona/index.style.js
+++ client/src/Component/views/Persona/index.style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Description = styled.p`
-margin-top: 4rem;
-font-size: 1.9rem;
-font-weight: normal;
+  margin-top: 4rem;
+  font-size: 1.9rem;
+  font-weight: normal;
 `;
+
