b1b5e74ce25e8ab73c3ec547db1c7310b2866653
hshahwan <haneen.shahwan9@gmail.com>
2019-04-24 17:02:27 +0300

    check indexof Slides and change the vaule we clicked

--- client/src/Component/views/Persona/index.js
+++ client/src/Component/views/Persona/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from 'nuka-carousel';
+import Swal from 'sweetalert2';
 import { PrevBtn, NextBtn } from '../Methods/index.style';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
@@ -9,10 +10,22 @@ import Button from '../../Common/Button';
 import PersonaData from '../../../fakeData/persona';
 
 class Persona extends Component {
-  state = {};
+  state = {
+    slideIndex: 0,
+  };
+
+  afterSlide = slideIndex => {
+    this.setState({ slideIndex });
+  };
+
+  handelIndexSlide = () => {
+    const { slideIndex } = this.state;
+    this.setState({ slideIndex: !slideIndex });
+    Swal.fire('you select this persona');
+  };
 
   render() {
-  // need validation//
+    // need validation//
     const {
       heightCard,
       heightImg,
@@ -21,35 +34,40 @@ class Persona extends Component {
       widthtCard,
       buttonText,
     } = this.props;
+    const { slideIndex } = this.state;
     return (
       <div>
         <Header headerImg={null} titleText="Selection your persona" />
-        <Carousel
-          enableKeyboardControls
-          renderCenterLeftControls={({ previousSlide }) => (
-            <PrevBtn onClick={previousSlide}>.</PrevBtn>
-          )}
-          renderCenterRightControls={({ nextSlide }) => (
-            <NextBtn onClick={nextSlide}>.</NextBtn>
-          )}
-        >
-          {PersonaData.map(element => {
-            return (
-              <CardWrapper
-                heightCard="45.5rem"
-                widthtCard="20rem"
-                key={element.id}
-              >
-                <Img heightImg="19.6rem" src={element.img} alt="PersonImg" />
-                <Info heightInfoCard="30rem">
-                  <CardTitle textAlign="center">{element.title}</CardTitle>
-                  <Description>{element.text}</Description>
-                </Info>
-              </CardWrapper>
-            );
-          })}
-        </Carousel>
-        <Button buttonText="Choose" />
+        <div>
+          <Carousel
+            slideIndex={this.slideIndex}
+            afterSlide={this.afterSlide}
+            enableKeyboardControls
+            renderCenterLeftControls={({ previousSlide }) => (
+              <PrevBtn onClick={previousSlide}>.</PrevBtn>
+            )}
+            renderCenterRightControls={({ nextSlide }) => (
+              <NextBtn onClick={nextSlide}>.</NextBtn>
+            )}
+          >
+            {PersonaData.map(element => {
+              return (
+                <CardWrapper
+                  heightCard="45.5rem"
+                  widthtCard="20rem"
+                  key={element.id}
+                >
+                  <Img heightImg="19.6rem" src={element.img} alt="PersonImg" />
+                  <Info heightInfoCard="30rem">
+                    <CardTitle textAlign="center">{element.title}</CardTitle>
+                    <Description>{element.text}</Description>
+                  </Info>
+                  <Button buttonText="Choose" onClick={this.handelIndexSlide} />
+                </CardWrapper>
+              );
+            })}
+          </Carousel>
+        </div>
         <Footer backLink="/yourScenario" nextLink="/" reviewScenario={false} />
       </div>
     );
