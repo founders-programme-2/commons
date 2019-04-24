709c6ec1e67ba25d1e20e591726911df8808f90c
Kristina Jaggard <jaggardk@gmail.com>
2019-04-24 19:24:52 +0100

    method card data taken from: https://quip.com/gfarAwww2dyn/Commons-6

---
+++ client/src/fakeData/methodCardData.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    id: 0,
+    cardTitle: 'Directory',
+    cardImage: null,
+    description:
+      'Lists local organisations and services following research. Needs database.',
+    difficulty: 'TBA',
+    category: 'finding',
+    use: 'TBA',
+    resourcePoints: 2,
+  },
+  {
+    id: 1,
+    cardTitle: 'Location Map',
+    description:
+      'Shows favourite local places and useful organisations. Needs database.',
+    cardImage: null,
+    difficulty: 'TBA',
+    category: 'finding',
+    use: 'TBA',
+    resourcePoints: 2,
+  },
+  {
+    id: 2,
+    cardTitle: 'Network map',
+    description:
+      'Shows who knows who, and how groups and projects connect, with information about people\'s needs, skills and interests. Needs database',
+    cardImage: null,
+    difficulty: 'TBA',
+    category: 'finding',
+    use: 'TBA',
+    resourcePoints: 2,
+  },
+];
