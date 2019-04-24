7eed75f698ae07790ce1dcb3fd4bbc70a14c1c85
hshahwan <haneen.shahwan9@gmail.com>
2019-04-24 12:37:04 +0300

    preaper card component for person

--- client/src/Component/views/Persona/index.js
+++ client/src/Component/views/Persona/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
-import { CardWrapper, Img, CardTitle } from '../MethodCard/index.style';
+import { Description } from './index.style';
+import { CardWrapper, Img, CardTitle, Info } from '../MethodCard/index.style';
 import logo from '../../../assets/logo.svg';
 
 const Persona = props => {
-  const { heightCard, heightImg, textAlign } = props;
+  const { heightCard, heightImg, textAlign, heightInfoCard,widthtCard } = props;
   return (
     <div>
       <Header headerImg={null} titleText="Selection your persona" />
-      <CardWrapper heightCard="45.5rem">
+      <CardWrapper heightCard="45.5rem" widthtCard="25rem">
         <Img heightImg="19.6rem" src={logo} alt="PersonImg" />
-        <CardTitle textAlign="center">Sarah</CardTitle>
+        <Info heightInfoCard="30rem">
+          <CardTitle textAlign="center">Sarah</CardTitle>
+          <Description>
+            Is a single mun with a young baby.She would like to access the
+            enternet but thinks the connections and equipment would be too
+            expensive
+          </Description>
+        </Info>
       </CardWrapper>
       <Footer backLink="/yourScenario" nextLink="/" reviewScenario={false} />
     </div>
