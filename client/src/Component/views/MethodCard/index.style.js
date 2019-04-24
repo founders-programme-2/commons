b017e3818bdf330c07cfd384ea34f574b0747d23
hshahwan <haneen.shahwan9@gmail.com>
2019-04-24 12:18:43 +0300

    update hight for info card as props

--- client/src/Component/views/MethodCard/index.style.js
+++ client/src/Component/views/MethodCard/index.style.js
@@ -28,8 +28,8 @@ export const Info = styled.section`
   padding: 1rem;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 0px 0px 5px 5px;
-  width: ${props => props.widthInfoCard || '22rem'}; 
-  height: 13.5rem;
+  width: ${props => props.widthInfoCard || '22rem'};
+  height: ${props => props.heightInfoCard || '13.5rem'};
 `;
 
 export const CardTitle = styled.h2`
