3617ce61a8d5f86d79a8a90c35923ad0d6c4af38
ali <ali.7@outlook.com>
2019-04-24 19:37:22 +0300

    remove-Prioritize-section-from-page relate #64

--- client/src/Component/views/PrioritiesTools/index.js
+++ client/src/Component/views/PrioritiesTools/index.js
@@ -3,19 +3,7 @@ import Carousel from 'nuka-carousel';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
 import MethodCard from '../MethodCard';
-import {
-  PrevBtn,
-  NextBtn,
-  Line,
-  Title,
-  FormWrap,
-  RadioWrap,
-  Input,
-  TimelineWrap,
-  Label,
-  Span,
-  LightSpan,
-} from './index.style';
+import { PrevBtn, NextBtn } from './index.style';
 
 class PrioritiesTools extends Component {
   state = {};
@@ -42,57 +30,9 @@ class PrioritiesTools extends Component {
               removeMethod={null}
               errorOverSpend={null}
               tools
+              priority
             />
           </Carousel>
-          <div>
-            <Title>Prioritize:</Title>
-            <Line />
-            <FormWrap>
-              <RadioWrap>
-                <Label htmlFor="low">
-                  <Input type="radio" id="low" name="choose" />
-                  Low
-                </Label>
-              </RadioWrap>
-              <RadioWrap>
-                <Label htmlFor="medium">
-                  <Input type="radio" id="medium" name="choose" />
-                  Medium
-                </Label>
-              </RadioWrap>
-              <RadioWrap>
-                <Label htmlFor="high">
-                  <Input type="radio" id="high" name="choose" />
-                  High
-                </Label>
-              </RadioWrap>
-            </FormWrap>
-          </div>
-          <Line />
-          <TimelineWrap>
-            <Title>Timeline of implementation:</Title>
-            <Line />
-            <FormWrap>
-              <RadioWrap>
-                <Label htmlFor="short">
-                  <Input type="radio" id="short" name="time-choose" />
-                  <Span>short-term:</Span> <LightSpan>30 days</LightSpan>
-                </Label>
-              </RadioWrap>
-              <RadioWrap>
-                <Label htmlFor="mid">
-                  <Input type="radio" id="mid" name="time-choose" />
-                  <Span>mid-term:</Span> <LightSpan>6 months</LightSpan>
-                </Label>
-              </RadioWrap>
-              <RadioWrap>
-                <Label htmlFor="long">
-                  <Input type="radio" id="long" name="time-choose" />
-                  <Span>long-term:</Span> <LightSpan>+6 months</LightSpan>
-                </Label>
-              </RadioWrap>
-            </FormWrap>
-          </TimelineWrap>
         </div>
         <Footer
           backLink="/priorities"
