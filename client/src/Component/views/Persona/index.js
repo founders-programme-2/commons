8a41e9b9b589c365c8f550b2e5990dd3353ad0df
hshahwan <haneen.shahwan9@gmail.com>
2019-04-24 15:24:37 +0300

    change the component from Stateless to Stateful

--- client/src/Component/views/Persona/index.js
+++ client/src/Component/views/Persona/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Carousel from 'nuka-carousel';
 import { PrevBtn, NextBtn } from '../Methods/index.style';
 import Header from '../../Common/Header';
@@ -8,40 +8,52 @@ import { CardWrapper, Img, CardTitle, Info } from '../MethodCard/index.style';
 import Button from '../../Common/Button';
 import PersonaData from '../../../fakeData/persona';
 
-const Persona = props => {
-  const { heightCard, heightImg, textAlign, heightInfoCard,widthtCard, buttonText } = props;
-  return (
-    <div>
-      <Header headerImg={null} titleText="Selection your persona" />
-      <Carousel
-        enableKeyboardControls
-        renderCenterLeftControls={({ previousSlide }) => (
-          <PrevBtn onClick={previousSlide}>.</PrevBtn>
-        )}
-        renderCenterRightControls={({ nextSlide }) => (
-          <NextBtn onClick={nextSlide}>.</NextBtn>
-        )}
-      >
-        {PersonaData.map(element => {
-          return (
-            <CardWrapper
-              heightCard="45.5rem"
-              widthtCard="20rem"
-              key={element.id}
-            >
-              <Img heightImg="19.6rem" src={element.img} alt="PersonImg" />
-              <Info heightInfoCard="30rem">
-                <CardTitle textAlign="center">{element.title}</CardTitle>
-                <Description>{element.text}</Description>
-              </Info>
-            </CardWrapper>
-          );
-        })}
-      </Carousel>
-      <Button buttonText="Choose" />
-      <Footer backLink="/yourScenario" nextLink="/" reviewScenario={false} />
-    </div>
-  );
-};
+class Persona extends Component {
+  state = {};
+
+  render() {
+  // need validation//
+    const {
+      heightCard,
+      heightImg,
+      textAlign,
+      heightInfoCard,
+      widthtCard,
+      buttonText,
+    } = this.props;
+    return (
+      <div>
+        <Header headerImg={null} titleText="Selection your persona" />
+        <Carousel
+          enableKeyboardControls
+          renderCenterLeftControls={({ previousSlide }) => (
+            <PrevBtn onClick={previousSlide}>.</PrevBtn>
+          )}
+          renderCenterRightControls={({ nextSlide }) => (
+            <NextBtn onClick={nextSlide}>.</NextBtn>
+          )}
+        >
+          {PersonaData.map(element => {
+            return (
+              <CardWrapper
+                heightCard="45.5rem"
+                widthtCard="20rem"
+                key={element.id}
+              >
+                <Img heightImg="19.6rem" src={element.img} alt="PersonImg" />
+                <Info heightInfoCard="30rem">
+                  <CardTitle textAlign="center">{element.title}</CardTitle>
+                  <Description>{element.text}</Description>
+                </Info>
+              </CardWrapper>
+            );
+          })}
+        </Carousel>
+        <Button buttonText="Choose" />
+        <Footer backLink="/yourScenario" nextLink="/" reviewScenario={false} />
+      </div>
+    );
+  }
+}
 
 export default Persona;
