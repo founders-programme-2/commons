f9c5800ce857d256bcaf082b3e95b8ce4a30f151
hshahwan <haneen.shahwan9@gmail.com>
2019-04-24 12:08:32 +0300

    import text-align and passing center value for title

--- client/src/Component/views/Persona/index.js
+++ client/src/Component/views/Persona/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
-import { CardWrapper } from '../MethodCard/index.style';
+import { CardWrapper, Img, CardTitle } from '../MethodCard/index.style';
+import logo from '../../../assets/logo.svg';
 
-const Persona = () => {
+const Persona = props => {
+  const { heightCard, heightImg, textAlign } = props;
   return (
     <div>
       <Header headerImg={null} titleText="Selection your persona" />
-      <CardWrapper>
-        <div>Haneen</div>
+      <CardWrapper heightCard="45.5rem">
+        <Img heightImg="19.6rem" src={logo} alt="PersonImg" />
+        <CardTitle textAlign="center">Sarah</CardTitle>
       </CardWrapper>
       <Footer backLink="/yourScenario" nextLink="/" reviewScenario={false} />
     </div>
