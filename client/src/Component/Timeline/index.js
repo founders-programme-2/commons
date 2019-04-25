99558583858cf118a6f51552ac14f100ea45dc52
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-04-25 10:56:02 +0100

    Add headings to timeline and style appropriately

--- client/src/Component/Timeline/index.js
+++ client/src/Component/Timeline/index.js
@@ -6,32 +6,26 @@ import {
   MediumTermDiv,
   LongTermDiv,
   CardContainerDiv,
+  CardContainer,
+  SubtitleTimeline,
+  SubtitlePriority,
+  SubtitleTime,
 } from './index.style';
 import TimelineCard from './TimelineCards';
 
 const exampleData = [
   {
-    cardTitle: '1p1t',
+    cardTitle: 'Email Group',
     priority: 1,
     time: 1,
   },
   {
-    cardTitle: '1p1t',
-    priority: 1,
-    time: 1,
-  },
-  {
-    cardTitle: '1p1t',
-    priority: 1,
-    time: 1,
-  },
-  {
-    cardTitle: '2p2t',
+    cardTitle: 'Network Map',
     priority: 2,
     time: 2,
   },
   {
-    cardTitle: '3p1t',
+    cardTitle: 'Example',
     priority: 3,
     time: 1,
   },
@@ -48,18 +42,28 @@ const Timeline = props => {
   };
   return (
     <div>
+      <SubtitleTimeline>Timeline</SubtitleTimeline>
       <Arrow src={arrowPath} />
       <ShortTermDiv>
-        <CardContainerDiv>{renderElements(1, 1)}</CardContainerDiv>
+        <SubtitleTime>Short-Term</SubtitleTime>
+        <CardContainerDiv>
+          <SubtitlePriority>Low priority</SubtitlePriority>
+          {renderElements(1, 1)}
+        </CardContainerDiv>
         <CardContainerDiv>{renderElements(2, 1)}</CardContainerDiv>
-        <CardContainerDiv>{renderElements(3, 1)}</CardContainerDiv>
+        <CardContainerDiv>
+          <SubtitlePriority>High priority</SubtitlePriority>
+          {renderElements(3, 1)}
+        </CardContainerDiv>
       </ShortTermDiv>
       <MediumTermDiv>
+        <SubtitleTime>Med-Term</SubtitleTime>
         <CardContainerDiv>{renderElements(1, 2)}</CardContainerDiv>
         <CardContainerDiv>{renderElements(2, 2)}</CardContainerDiv>
         <CardContainerDiv>{renderElements(3, 2)}</CardContainerDiv>
       </MediumTermDiv>
       <LongTermDiv>
+        <SubtitleTime>Long-Term</SubtitleTime>
         <CardContainerDiv>{renderElements(1, 3)}</CardContainerDiv>
         <CardContainerDiv>{renderElements(2, 3)}</CardContainerDiv>
         <CardContainerDiv>{renderElements(3, 3)}</CardContainerDiv>
--- client/src/Component/Timeline/index.style.js
+++ client/src/Component/Timeline/index.style.js
@@ -11,7 +11,7 @@ export const Arrow = styled.img`
   right: 0;
 `;
 
-export const ShortTermDiv = styled.div`
+export const TermDiv = styled.div`
   background-color: rgba(255, 199, 41, 0.25);
   min-height: 15rem;
   width: 100vw;
@@ -20,11 +20,15 @@ export const ShortTermDiv = styled.div`
   flex-direction: row;
 `;
 
-export const MediumTermDiv = styled(ShortTermDiv)`
+export const ShortTermDiv = styled(TermDiv)`
+  padding-top: 2rem;
+`;
+
+export const MediumTermDiv = styled(TermDiv)`
   background-color: rgba(255, 199, 41, 0.5);
 `;
 
-export const LongTermDiv = styled(ShortTermDiv)`
+export const LongTermDiv = styled(TermDiv)`
   background-color: rgba(255, 199, 41, 0.75);
 `;
 
@@ -36,4 +40,43 @@ export const CardContainerDiv = styled.div`
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
+`;
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const Subtitle = styled.h3`
+  font-family: Poppins;
+  font-weight: 800;
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+export const SubtitleTimeline = styled(Subtitle)`
+  margin: 0 0 3.5rem 0;
+`;
+
+export const SubtitlePriority = styled(Subtitle)`
+  margin-top: -2rem;
+  width: 100%;
+  align-self: flex-start;
+`;
+
+export const SubtitleTime = styled(Subtitle)`
+  -webkit-transform: rotate(270deg);
+  -moz-transform: rotate(270deg);
+  -o-transform: rotate(270deg);
+  -ms-transform: rotate(270deg);
+  transform: rotate(270deg);
+  white-space: nowrap;
+  display: block;
+  height: 2rem;
+  width: 7rem;
+  margin-left: -2rem;
+  align-self: center;
 `;
