00fccde86db8b55c4f9c340088dcbe1ea86ba42b
hshahwan <haneen.shahwan9@gmail.com>
2019-04-25 13:42:13 +0300

    add-font-wieght also adding font-size for title

--- client/src/Component/views/KeyTerms/index.style.js
+++ client/src/Component/views/KeyTerms/index.style.js
@@ -7,13 +7,18 @@ export const WrapperTitlte = styled.div`
   flex-direction: row;
 `;
 export const Img = styled.img`
-  height: 4rem;
-  width: 4rem;
-  margin-right: 1rem;
+  height: 3.4rem;
+  width: 3.6rem;
 `;
 
 export const Paragraph = styled.p`
-  font-size: 1.5rem;
+  font-size: 1.4rem;
   font-family: 'Open Sans';
   text-align: center;
+  font-weight: normal;
+  margin-bottom: 1rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.7rem;
 `;
