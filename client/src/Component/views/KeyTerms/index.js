f6ce919dc5941ae53c1618bbc9b5d18eb8cb275b
hshahwan <haneen.shahwan9@gmail.com>
2019-04-25 12:38:58 +0300

    use styled component for the first-key-terms;

--- client/src/Component/views/KeyTerms/index.js
+++ client/src/Component/views/KeyTerms/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
 import key from '../../../assets/key.svg';
+import logo from '../../../assets/logo.svg';
+import { WrapperTitlte, Img, Paragraph } from './index.style';
 
 const KeyTerms = () => {
   return (
     <div>
       <Header headerImg={key} titleText="Key Terms" />
-      <h3>Hub</h3>
-      <p>
-        - An organising group that may have access to a physical space that
-        fosters communication and collaboration
-      </p>
-      <br />
-      <p>
-        - An example might be a community center or a charity with an office
-      </p>
+      <WrapperTitlte>
+        <Img src={logo} alt="keyhub" />
+        <h1>Hub</h1>
+      </WrapperTitlte>
+      <Paragraph>
+        An organising group that may have access to a physical space that
+        fosters communication and collaboration.An example might be a community
+        center or a charity with an office
+      </Paragraph>
       <Footer />
     </div>
   );
