6428d7332c31038c7a1ad3f1791e60be4fe53a41
hshahwan <haneen.shahwan9@gmail.com>
2019-04-28 09:28:57 +0300

    update the data for real persona

--- client/src/fakeData/persona.js
+++ client/src/fakeData/persona.js
@@ -1,23 +1,23 @@
 module.exports = [
   {
     id: '1',
-    img: 'https://i.imgur.com/eAYxg4q.png',
-    title: 'Sarah',
+    img: 'https://i.imgur.com/ifAbUdp.png',
+    title: 'Mark',
     text:
       'Is a single mun with a young baby.She would like to access theenternet but thinks the connections and equipment would be tooexpensiv ',
   },
   {
     id: '2',
-    img: 'https://i.imgur.com/eAYxg4q.png',
-    title: 'Haneen',
+    img: 'https://i.imgur.com/pDFK9Ou.png',
+    title: 'Shirley',
     text:
-      'Is a single mun with a young baby.She would like to access theenternet but thinks the connections and equipment would be tooexpensiv ',
+      'An experienced community involvement worker based at a local infrastructure organization,Shirley lives locally and her child attends as local secondary school.She has very good people skills and understanding of local needs.',
   },
   {
     id: '3',
-    img: 'https://i.imgur.com/eAYxg4q.png',
-    title: 'Swsan',
+    img: 'https://i.imgur.com/Yh4FlTb.png',
+    title: 'Jake',
     text:
-      'Is a single mun with a young baby.She would like to access theenternet but thinks the connections and equipment would be tooexpensiv ',
+      'Jake manages the ABCD programme, and has community and organizational development experience.He now has to turn the version of slipham living lab into reality.',
   },
 ];
