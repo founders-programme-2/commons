68d8473ff84d78aa097d6e55ace778abc28dd74a
Kristina Jaggard <jaggardk@gmail.com>
2019-04-29 11:54:27 +0100

    add shadow to prev & next button / update font styling on scenario btn

--- client/src/Component/Common/Footer/Footer.style.js
+++ client/src/Component/Common/Footer/Footer.style.js
@@ -5,6 +5,7 @@ export const Buttons = styled.div`
   margin-top: 40.5rem;
   justify-content: space-between;
   padding: 0.9rem;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.5));
 `;
 
 export const PreviousButton = styled.button`
@@ -15,38 +16,29 @@ export const PreviousButton = styled.button`
   font-size: 1rem;
   border: none;
   border-radius: 0.5rem;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   height: 4.9rem;
 `;
 
-export const NextButton = styled.button`
-  width: 5.2rem;
-  clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
-  background: #faa634;
+export const NextButton = styled(PreviousButton)`
   transform: rotate(90deg);
-  font-size: 1rem;
-  border: none;
-  border-radius: 0.5rem;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  height: 4.9rem;
 `;
 
 export const BackToScenario = styled.button`
-  background: #faa634;
+  background: var(--button-background-color);
   height: 4.9rem;
   width: 12.5rem;
   border: none;
-  font-size: 1.1rem;
+  font-size: 1.5rem;
   font-weight: 1rem;
+  line-height: 1.3rem;
   border-radius: 0.5rem;
   color: black;
   font-weight: bold;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: 'Poppins';
 `;
 
 export const Line = styled.div`
   margin-bottom: -40rem;
   border: 0.3rem solid #faa634;
   border-radius: 1rem;
-  /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
 `;
--- client/src/Component/Common/Footer/index.js
+++ client/src/Component/Common/Footer/index.js
@@ -7,6 +7,8 @@ import {
   PreviousButton,
   BackToScenario,
   Line,
+  PrevBtnShadow,
+  NextBtnShadow,
 } from './Footer.style';
 
 const Footer = ({ nextLink, backLink, reviewScenario }) => {
@@ -15,7 +17,12 @@ const Footer = ({ nextLink, backLink, reviewScenario }) => {
       <Line />
       <Buttons>
         <PreviousButton as={Link} to={backLink} type="button" />
-        {reviewScenario && <BackToScenario>Review Scenario</BackToScenario>}
+        {reviewScenario && (
+          <BackToScenario>
+            Review <br />
+            Scenario
+          </BackToScenario>
+        )}
         {nextLink && <NextButton as={Link} to={nextLink} type="button" />}
       </Buttons>
     </Fragment>
