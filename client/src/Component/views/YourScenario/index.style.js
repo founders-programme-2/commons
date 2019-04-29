75dafbb87aeb682698d0bafc63622af94c5d6933
hshahwan <haneen.shahwan9@gmail.com>
2019-04-29 20:55:58 +0300

    add line between scenario and personas

---
+++ client/src/Component/views/YourScenario/index.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Description = styled.p`
+  margin-top: 4rem;
+  font-size: 1.6rem;
+  font-weight: normal;
+  text-align: center;
+  font-family: 'Open Sans', sans-serif;
+`;
+
+export const HeadLine = styled.p`
+  margin-top: 2.5rem;
+  text-align: center;
+  font-size: 17px;
+  font-family: 'Open Sans', sans-serif;
+`;
