a7e32eb1285bbd065d08afe931c5523fb0e79c96
Kristina Jaggard <jaggardk@gmail.com>
2019-04-29 15:52:15 +0100

    implement alerts for trying to move forward without selecting any resources and trying to move forward without spending all your resources

--- client/src/Component/views/Methods/index.js
+++ client/src/Component/views/Methods/index.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import Carousel from 'nuka-carousel';
-import { Redirect, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import ReactRouterPropTypes from 'react-router-prop-types';
 import Header from '../../Common/Header';
 import MethodCard from '../MethodCard';
 import methodCardData from '../../../fakeData/methodCardData';
+import { MyContext } from '../../../Context/ContextComponent';
 import {
   RemainingResources,
   Line,
@@ -19,7 +21,6 @@ import {
 class Methods extends Component {
   state = {
     resources: 15,
-    redirect: false,
     data: methodCardData,
   };
 
@@ -65,17 +66,35 @@ class Methods extends Component {
     });
   };
 
-  noSelectedCardsError = () => {
-    const { selectedCards } = this.context;
+  // Triggers alerts on next click
+  nextAlerts = (selectedCards, resources) => {
     if (selectedCards.length === 0) {
       Swal.fire({
         type: 'error',
         title: 'Oops...',
         text: 'You must select your methods first!',
         confirmButtonText: 'OK',
+        confirmButtonColor: '#faa634',
       }).then(() => {
         const { history } = this.props;
         history.push('/methods');
+      });
+    } else if (resources > 0) {
+      Swal.fire({
+        type: 'info',
+        title: 'Are you sure you want to move forward?',
+        text: 'You still have resources left to spend!',
+        confirmButtonText: 'Go back and spend more resources.',
+        confirmButtonColor: '#faa634',
+        showCancelButton: true,
+        cancelButtonText: "Let's move on.",
+      }).then(value => {
+        const { history } = this.props;
+        if (value.dismiss === 'cancel') {
+          history.push('/priorities');
+        } else if (value.value === true) {
+          history.push('/methods');
+        }
       });
     }
   };
@@ -83,40 +102,60 @@ class Methods extends Component {
   render() {
     const { resources } = this.state;
     return (
-      <Fragment>
-        {this.renderRedirect()}
-        <Header headerImg={null} titleText="Select your methods" />
-        <Carousel
-          wrapAround
-          enableKeyboardControls
-          renderCenterLeftControls={({ previousSlide }) => (
-            <PrevBtn onClick={previousSlide}>.</PrevBtn>
-          )}
-          renderCenterRightControls={({ nextSlide }) => (
-            <NextBtn onClick={nextSlide}>.</NextBtn>
-          )}
-        >
-          {this.renderMethodCards()}
-        </Carousel>
-        <Line />
+      <MyContext.Consumer>
+        {context => {
+          const { selectedCards } = context;
+          return (
+            <Fragment>
+              <Header headerImg={null} titleText="Select your methods" />
+              <Carousel
+                wrapAround
+                enableKeyboardControls
+                renderCenterLeftControls={({ previousSlide }) => (
+                  <PrevBtn onClick={previousSlide}>.</PrevBtn>
+                )}
+                renderCenterRightControls={({ nextSlide }) => (
+                  <NextBtn onClick={nextSlide}>.</NextBtn>
+                )}
+              >
+                {this.renderMethodCards()}
+              </Carousel>
+              <Line />
 
-        <RemainingResources>
-          Remaining resources: <span>&nbsp;</span>
-          {resources}
-        </RemainingResources>
+              <RemainingResources>
+                Remaining resources: <span>&nbsp;</span>
+                {resources}
+              </RemainingResources>
 
-        <Line />
-        <Footer>
-          <FooterPrevious as={Link} to="./play" type="button" />
-          <BackToScenario>
-            Review <br />
-            Scenario
-          </BackToScenario>
-          <FooterNext as={Link} to="/priorities" type="button" onClick={this./>
-        </Footer>
-      </Fragment>
+              <Line />
+              <Footer>
+                <FooterPrevious as={Link} to="./play" type="button" />
+                <BackToScenario>
+                  Review <br />
+                  Scenario
+                </BackToScenario>
+                <FooterNext
+                  as={Link}
+                  to="/priorities"
+                  type="button"
+                  onClick={event => {
+                    event.preventDefault();
+                    this.nextAlerts(selectedCards, resources);
+                  }}
+                />
+              </Footer>
+            </Fragment>
+          );
+        }}
+      </MyContext.Consumer>
     );
   }
 }
+
+Methods.propTypes = {
+  history: ReactRouterPropTypes.history.isRequired,
+};
+
+Methods.contextType = MyContext;
 
 export default Methods;
