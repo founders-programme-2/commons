ab3af2462ef1f8660ca068f56a589634da21122a
hshahwan <haneen.shahwan9@gmail.com>
2019-04-29 20:55:07 +0300

    add persona section under the scenario

--- client/src/Component/views/YourScenario/index.js
+++ client/src/Component/views/YourScenario/index.js
@@ -1,38 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Carousel from 'nuka-carousel';
+import { PrevBtn, NextBtn } from '../Methods/index.style';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
+import { Description, HeadLine } from './index.style';
+import { CardWrapper, Img, CardTitle, Info } from '../MethodCard/index.style';
 import { Content } from '../About/index.style';
+import { Line } from '../../Common/Header/index.style';
+import PersonaData from '../../../fakeData/persona';
 import houses from '../../../assets/houses.svg';
 
-const YourScenario = () => {
-  return (
-    <div>
-      <Header headerImg={houses} titleText="Your scenario" />
-      <Content>
-        <p>
-          The Slipham social action centre is launching a Connected Commons
-          programme, aiming to help people explore its heritage, find local
-          opportunities and services, and support new projects for a better
-          Slipham in the future. It will do this by mapping people, projects and
-          groups to find what they can offer, what they need, and how they are
-          connected at present. The Commons won&apos;t be just one physical
-          space. It will be on-the-ground trails and gathering points, maps of
-          doing what, a website and other communication tools, with guides for
-          action. We&apos;ll have small grants to support projects, and help on
-          fundraising. The overall aim is to create an environment within which
-          connections and collaborations can take place - the Slipham Connected
-          Commons.
-        </p>
-        <br />
-        <p style={{ fontWeight: 'bold' }}>
-          Your goal is to select the tools that you would use to faciliate
-          creating this Connected Commons Program. You have a limited amount of
-          resources and must choose wisely.
-        </p>
-      </Content>
-      <Footer backLink="/play" nextLink="/persona" reviewScenario={false} />
-    </div>
-  );
-};
+class YourScenario extends Component {
+  state = {
+    slideIndex: 0,
+    // checked: false,
+  };
+
+  afterSlide = slideIndex => {
+    this.setState({ slideIndex });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header headerImg={houses} titleText="Your scenario" />
+        <Content>
+          <p>
+            The Slipham social action centre is launching a Connected Commons
+            programme, aiming to help people explore its heritage, find local
+            opportunities and services, and support new projects for a better
+            Slipham in the future. It will do this by mapping people, projects
+            and groups to find what they can offer, what they need, and how they
+            are connected at present. The Commons won&apos;t be just one
+            physical space. It will be on-the-ground trails and gathering
+            points, maps of doing what, a website and other communication tools,
+            with guides for action. We&apos;ll have small grants to support
+            projects, and help on fundraising. The overall aim is to create an
+            environment within which connections and collaborations can take
+            place - the Slipham Connected Commons.
+          </p>
+          <br />
+          <p style={{ fontWeight: 'bold' }}>
+            Your goal is to select the tools that you would use to faciliate
+            creating this Connected Commons Program. You have a limited amount
+            of resources and must choose wisely.
+          </p>
+        </Content>
+        <Line />
+        <HeadLine>
+          Here are some potential user personas that you can keep in mind while
+          while you choose your methods.
+        </HeadLine>
+        <div>
+          <Carousel
+            wrapAround
+            slideIndex={this.slideIndex}
+            afterSlide={this.afterSlide}
+            enableKeyboardControls
+            renderCenterLeftControls={({ previousSlide }) => (
+              <PrevBtn onClick={previousSlide}>.</PrevBtn>
+            )}
+            renderCenterRightControls={({ nextSlide }) => (
+              <NextBtn onClick={nextSlide}>.</NextBtn>
+            )}
+          >
+            {PersonaData.map(element => {
+              return (
+                <CardWrapper key={element.id}>
+                  <Img heightImg="20rem" src={element.img} alt="PersonImg" />
+                  <Info>
+                    <CardTitle textAlign="center">{element.title}</CardTitle>
+                    <Description>{element.text}</Description>
+                  </Info>
+                </CardWrapper>
+              );
+            })}
+          </Carousel>
+        </div>
+        <Footer
+          backLink="/yourScenario"
+          nextLink="/challenges"
+          reviewScenario={false}
+        />
+      </div>
+    );
+  }
+}
 
 export default YourScenario;
