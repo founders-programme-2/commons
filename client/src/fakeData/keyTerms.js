4b0fb03fe9f1b2fe58c27ab2901b7fc27aac852c
hshahwan <haneen.shahwan9@gmail.com>
2019-04-29 11:39:06 +0300

    add space after period

--- client/src/fakeData/keyTerms.js
+++ client/src/fakeData/keyTerms.js
@@ -4,7 +4,7 @@ module.exports = [
     title: 'Hub',
     img: 'https://i.imgur.com/rPJyipK.png',
     content:
-      'An organising group that may have access to a physical space that fosters communication and collaboration.An example might be a community center or a charity with an office.',
+      'An organising group that may have access to a physical space that fosters communication and collaboration. An example might be a community center or a charity with an office.',
   },
   {
     id: '2',
@@ -24,6 +24,6 @@ module.exports = [
     title: 'Networks   ',
     img: 'https://i.imgur.com/rPJyipK.png',
     content:
-      'Groups of various organisations that work together for common causes.Often requires a membership.Network mapping makes these relationships visible and trackable.',
+      'Groups of various organisations that work together for common causes. Often requires a membership.Network mapping makes these relationships visible and trackable.',
   },
 ];
