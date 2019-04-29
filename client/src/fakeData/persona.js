55c836d95b9a64483efa5e43adf708b7395ab789
hshahwan <haneen.shahwan9@gmail.com>
2019-04-29 18:34:10 +0300

    add after periods

--- client/src/fakeData/persona.js
+++ client/src/fakeData/persona.js
@@ -4,20 +4,20 @@ module.exports = [
     img: 'https://i.imgur.com/ifAbUdp.png',
     title: 'Mark',
     text:
-      'Is a single mun with a young baby.She would like to access theenternet but thinks the connections and equipment would be tooexpensiv ',
+      'An experienced community development worker, Mark lives in Slipham with his family. His children attend local schools and he is very well connected to the local community in Golden. Mark is passionate about sports and has a great understanding of the key statutory sector and health processes, with middle management contacts.',
   },
   {
     id: '2',
     img: 'https://i.imgur.com/pDFK9Ou.png',
     title: 'Shirley',
     text:
-      'An experienced community involvement worker based at a local infrastructure organization,Shirley lives locally and her child attends as local secondary school.She has very good people skills and understanding of local needs.',
+      'An experienced community involvement worker based at a local infrastructure organization, Shirley lives locally and her child attends as a local secondary school. She has very good people skills and understanding of local needs.',
   },
   {
     id: '3',
     img: 'https://i.imgur.com/Yh4FlTb.png',
     title: 'Jake',
     text:
-      'Jake manages the ABCD programme, and has community and organizational development experience.He now has to turn the version of slipham living lab into reality.',
+      'Jake manages the ABCD programme and has a community and organizational development experience. He now has to turn the version of the slipham living lab into reality.',
   },
 ];
