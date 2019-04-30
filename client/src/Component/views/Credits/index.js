f69d48e175052f306082cda5610bfab143b3de96
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-04-30 11:54:17 +0100

    Create basic credits page with links to all appropriate team members and organizations

---
+++ client/src/Component/views/Credits/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import Header from '../../Common/Header';
+import Footer from '../../Common/Footer';
+import {
+  Wrapper,
+  ListContainer,
+  Logo,
+  ListItem,
+  LogoLink,
+} from './index.style';
+import intersticiaLogo from '../../../assets/intersticia-logo-sml.png';
+
+const Credits = () => {
+  return (
+    <div>
+      <Header headerImg={null} titleText="Credits" />
+
+      <Wrapper>
+        <h2>Supported by</h2>
+        <div>
+          <LogoLink href="https://intersticia.org/" target="blank">
+            <Logo src={intersticiaLogo} />
+          </LogoLink>
+        </div>
+        <h2>Developed by</h2>
+        <ListContainer>
+          <ListItem>
+            <a href="https://github.com/hshahwan" target="blank">
+              Haneen
+            </a>
+          </ListItem>
+          <ListItem>
+            <a href="https://github.com/ali-7" target="blank">
+              Ali
+            </a>
+          </ListItem>
+          <ListItem>
+            <a href="https://github.com/teenie-quaggard" target="blank">
+              Kristina
+            </a>
+          </ListItem>
+          <ListItem>
+            <a href="https://github.com/oliversw" target="blank">
+              Oliver
+            </a>
+          </ListItem>
+        </ListContainer>
+        <h2>In conjunction with</h2>
+        <ListContainer>
+          <ListItem>
+            <a href="https://networkedcity.london/start" target="blank">
+              Networked City
+            </a>
+          </ListItem>
+          <ListItem>
+            <a href="https://www.foundersandcoders.com/" target="blank">
+              Founders and Coders
+            </a>
+          </ListItem>
+          <ListItem>
+            <a href="https://gazaskygeeks.com/" target="blank">
+              Gaza Sky Geeks
+            </a>
+          </ListItem>
+        </ListContainer>
+        <h2>Resources</h2>
+        <ListContainer>
+          <ListItem>
+            <a href="https://www.flaticon.com/" target="blank">
+              Flaticon
+            </a>
+          </ListItem>
+        </ListContainer>
+      </Wrapper>
+
+      <Footer backLink="/summary" nextLink="{null}" reviewScenario={null} />
+    </div>
+  );
+};
+
+export default Credits;
---
+++ client/src/Component/views/Credits/index.style.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ListContainer = styled.ul`
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const Logo = styled.img`
+  width: 40%;
+  height: auto;
+  margin: 0 auto;
+`;
+
+export const LogoLink = styled.a`
+  display: flex;
+  flex-direction: column;
+`;
