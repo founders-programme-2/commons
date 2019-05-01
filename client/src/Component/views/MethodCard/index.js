40b2faf5c4f46ee561544dbe55f1967e71e858e8
Charlie La Fosse <charlielafosse@outlook.com>
2019-05-01 17:14:19 +0100

    tidy up

--- client/src/Component/views/MethodCard/index.js
+++ client/src/Component/views/MethodCard/index.js
@@ -1,5 +1,5 @@
-import React, { Fragment, Component } from "react";
-import PropTypes from "prop-types";
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
 import { MyContext } from '../../../Context/ContextComponent';
 import {
   CardWrapper,
@@ -20,15 +20,15 @@ import {
   TimelineWrap,
   Label,
   Span,
-  LightSpan
-} from "./index.style";
-import star from "../../../assets/star.svg";
+  LightSpan,
+} from './index.style';
+import star from '../../../assets/star.svg';
 
 class CardComponent extends Component {
   state = {
     checked: false,
-    defaultPriority: "",
-    defaultTime: ""
+    defaultPriority: '',
+    defaultTime: '',
   };
 
   selectedPriority = event => {
@@ -85,179 +85,171 @@ class CardComponent extends Component {
       difficulty,
       id,
       tools,
-      priority
+      priority,
     } = this.props;
 
     const { checked, defaultPriority, defaultTime } = this.state;
-        const { addSelectedCard, removeSelectedCard } = this.context;
+    const { addSelectedCard, removeSelectedCard } = this.context;
     // saves func output to variable for render
     const starsRender = this.stars(resourcePoints);
     return (
-      <MyContext.Consumer>
-        {context => {
-          return (
-            <Fragment>
-              <CardWrapper>
-                {cardImg === null ? (
-                  <Img src={cardImg} alt="card logo" />
-                ) : (
-                  <DefaultImg alt="default card image" />
-                )}
-                <Info>
-                  <CardTitle>{cardTitle}</CardTitle>
-                  <br />
-                  <InfoText>{description}</InfoText>
-                  <br />
-                  <br />
-                  Requires: <InfoText>{requiredCards}</InfoText>
-                  <br />
-                  Difficulty: <InfoText>{difficulty}</InfoText>
-                  <br />
-                  Used by: <InfoText>{use}</InfoText>
-                  <br />
-                  Category: <InfoText>{category}</InfoText>
-                  <ResourcePoints>
-                    {resourcePoints} resource points
-                    <br />
-                  </ResourcePoints>
-                  <StarWrapper>{starsRender}</StarWrapper>
-                </Info>
-              </CardWrapper>
+      <Fragment>
+        <CardWrapper>
+          {cardImg === null ? (
+            <Img src={cardImg} alt="card logo" />
+          ) : (
+            <DefaultImg alt="default card image" />
+          )}
+          <Info>
+            <CardTitle>{cardTitle}</CardTitle>
+            <br />
+            <InfoText>{description}</InfoText>
+            <br />
+            <br />
+            Requires: <InfoText>{requiredCards}</InfoText>
+            <br />
+            Difficulty: <InfoText>{difficulty}</InfoText>
+            <br />
+            Used by: <InfoText>{use}</InfoText>
+            <br />
+            Category: <InfoText>{category}</InfoText>
+            <ResourcePoints>
+              {resourcePoints} resource points
+              <br />
+            </ResourcePoints>
+            <StarWrapper>{starsRender}</StarWrapper>
+          </Info>
+        </CardWrapper>
 
-              {!tools && !priority ? (
-                <UseResource>
-                  <label htmlFor="method-checkbox">
-                    Use this resource:
-                    <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <input
-                      data-testid={datatestid}
-                      id="method-checkbox"
-                      type="checkbox"
-                      checked={checked}
-                      onChange={event => {
-                        this.toggleCheckbox(event);
-                        if (checked === false) {
-                          removeMethod(resourcePoints, event);
-                          addSelectedCard(id);
-                          errorOverSpend();
-                        } else if (checked === true) {
-                          chooseMethod(resourcePoints, event);
-                          removeSelectedCard(id);
-                          errorOverSpend();
-                        }
-                      }}
+        {!tools && !priority ? (
+          <UseResource>
+            <label htmlFor="method-checkbox">
+              Use this resource:
+              <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+              <input
+                data-testid={datatestid}
+                id="method-checkbox"
+                type="checkbox"
+                checked={checked}
+                onChange={event => {
+                  this.toggleCheckbox(event);
+                  if (checked === false) {
+                    removeMethod(resourcePoints, event);
+                    addSelectedCard(id);
+                    errorOverSpend();
+                  } else if (checked === true) {
+                    chooseMethod(resourcePoints, event);
+                    removeSelectedCard(id);
+                    errorOverSpend();
+                  }
+                }}
+              />
+            </label>
+          </UseResource>
+        ) : null}
+
+        {tools && priority ? (
+          <Fragment>
+            <div>
+              <Title>Prioritize:</Title>
+              <Line />
+              <FormWrap>
+                <RadioWrap>
+                  <Label htmlFor="low">
+                    <Input
+                      data-id={id}
+                      type="radio"
+                      id="low"
+                      name="choose"
+                      value="low"
+                      checked={defaultPriority === 'low'}
+                      onChange={this.selectedPriority}
                     />
-                  </label>
-                </UseResource>
-              ) : null}
-
-              {tools && priority ? (
-                <Fragment>
-                  <div>
-                    <Title>Prioritize:</Title>
-                    <Line />
-                    <FormWrap>
-                      <RadioWrap>
-                        <Label htmlFor="low">
-                          <Input
-                            data-id={id}
-                            type="radio"
-                            id="low"
-                            name="choose"
-                            value="low"
-                            checked={defaultPriority === "low"}
-                            onChange={this.selectedPriority}
-                          />
-                          Low
-                        </Label>
-                      </RadioWrap>
-                      <RadioWrap>
-                        <Label htmlFor="medium">
-                          <Input
-                            data-id={id}
-                            type="radio"
-                            id="medium"
-                            name="choose"
-                            value="medium"
-                            checked={defaultPriority === 'medium'}
-                            onChange={this.selectedPriority}
-                          />
-                          Medium
-                        </Label>
-                      </RadioWrap>
-                      <RadioWrap>
-                        <Label htmlFor="high">
-                          <Input
-                            data-id={id}
-                            type="radio"
-                            id="high"
-                            name="choose"
-                            value="high"
-                            checked={defaultPriority === 'high'}
-                            onChange={this.selectedPriority}
-                          />
-                          High
-                        </Label>
-                      </RadioWrap>
-                    </FormWrap>
-                  </div>
-                  <Line />
-                  <TimelineWrap>
-                    <Title>Timeline of implementation:</Title>
-                    <Line />
-                    <FormWrap>
-                      <RadioWrap>
-                        <Label htmlFor="short">
-                          <Input
-                            data-id={id}
-                            type="radio"
-                            id="short"
-                            name="time-choose"
-                            value="short"
-                            checked={defaultTime === 'short'}
-                            onChange={this.selectedTime}
-                          />
-                          <Span>short-term:</Span>{' '}
-                          <LightSpan>30 days</LightSpan>
-                        </Label>
-                      </RadioWrap>
-                      <RadioWrap>
-                        <Label htmlFor="mid">
-                          <Input
-                            data-id={id}
-                            type="radio"
-                            id="mid"
-                            name="time-choose"
-                            value="mid"
-                            checked={defaultTime === 'mid'}
-                            onChange={this.selectedTime}
-                          />
-                          <Span>mid-term:</Span> <LightSpan>6 months</LightSpan>
-                        </Label>
-                      </RadioWrap>
-                      <RadioWrap>
-                        <Label htmlFor="long">
-                          <Input
-                            data-id={id}
-                            type="radio"
-                            id="long"
-                            name="time-choose"
-                            value="long"
-                            checked={defaultTime === 'long'}
-                            onChange={this.selectedTime}
-                          />
-                          <Span>long-term:</Span>{" "}
-                          <LightSpan>+6 months</LightSpan>
-                        </Label>
-                      </RadioWrap>
-                    </FormWrap>
-                  </TimelineWrap>
-                </Fragment>
-              ) : null}
-            </Fragment>
-          );
-        }}
-      </MyContext.Consumer>
+                    Low
+                  </Label>
+                </RadioWrap>
+                <RadioWrap>
+                  <Label htmlFor="medium">
+                    <Input
+                      data-id={id}
+                      type="radio"
+                      id="medium"
+                      name="choose"
+                      value="medium"
+                      checked={defaultPriority === 'medium'}
+                      onChange={this.selectedPriority}
+                    />
+                    Medium
+                  </Label>
+                </RadioWrap>
+                <RadioWrap>
+                  <Label htmlFor="high">
+                    <Input
+                      data-id={id}
+                      type="radio"
+                      id="high"
+                      name="choose"
+                      value="high"
+                      checked={defaultPriority === 'high'}
+                      onChange={this.selectedPriority}
+                    />
+                    High
+                  </Label>
+                </RadioWrap>
+              </FormWrap>
+            </div>
+            <Line />
+            <TimelineWrap>
+              <Title>Timeline of implementation:</Title>
+              <Line />
+              <FormWrap>
+                <RadioWrap>
+                  <Label htmlFor="short">
+                    <Input
+                      data-id={id}
+                      type="radio"
+                      id="short"
+                      name="time-choose"
+                      value="short"
+                      checked={defaultTime === 'short'}
+                      onChange={this.selectedTime}
+                    />
+                    <Span>short-term:</Span> <LightSpan>30 days</LightSpan>
+                  </Label>
+                </RadioWrap>
+                <RadioWrap>
+                  <Label htmlFor="mid">
+                    <Input
+                      data-id={id}
+                      type="radio"
+                      id="mid"
+                      name="time-choose"
+                      value="mid"
+                      checked={defaultTime === 'mid'}
+                      onChange={this.selectedTime}
+                    />
+                    <Span>mid-term:</Span> <LightSpan>6 months</LightSpan>
+                  </Label>
+                </RadioWrap>
+                <RadioWrap>
+                  <Label htmlFor="long">
+                    <Input
+                      data-id={id}
+                      type="radio"
+                      id="long"
+                      name="time-choose"
+                      value="long"
+                      checked={defaultTime === 'long'}
+                      onChange={this.selectedTime}
+                    />
+                    <Span>long-term:</Span> <LightSpan>+6 months</LightSpan>
+                  </Label>
+                </RadioWrap>
+              </FormWrap>
+            </TimelineWrap>
+          </Fragment>
+        ) : null}
+      </Fragment>
     );
   }
 }
@@ -277,7 +269,7 @@ CardComponent.propTypes = {
   use: PropTypes.string,
   category: PropTypes.arrayOf(PropTypes.string).isRequired,
   tools: PropTypes.bool.isRequired,
-  priority: PropTypes.bool.isRequired
+  priority: PropTypes.bool.isRequired,
 };
 
 CardComponent.defaultProps = {
@@ -287,7 +279,7 @@ CardComponent.defaultProps = {
   use: PropTypes.bool,
   chooseMethod: PropTypes.bool,
   removeMethod: PropTypes.bool,
-  errorOverSpend: PropTypes.bool
+  errorOverSpend: PropTypes.bool,
 };
 
 CardComponent.contextType = MyContext;
