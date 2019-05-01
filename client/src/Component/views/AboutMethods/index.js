6370b80dc8e9e04080feaf4e361e7cfb56930f11
Kristina Jaggard <jaggardk@gmail.com>
2019-05-01 17:20:06 +0100

    addition of copy for about methods

--- client/src/Component/views/AboutMethods/index.js
+++ client/src/Component/views/AboutMethods/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
 import { Content } from '../About/index.style';
@@ -9,8 +10,25 @@ const AboutMethods = () => {
       <Header headerImg={null} titleText="Choosing Methods" />
       <Content>
         <p>
+          In order to understand what collaborative tools may be useful, we need
+          to think on at least two dimensions:
+        </p>
+        <ul>
+          <li>
+            Who will use them, and where. For example by individuals on their
+            phones; in groups working on projects; or more widely in social
+            networks.
+          </li>
+          <li>
+            The main purpose of the tool: for finding information; sharing
+            privately; sharing publicly; for supporting cooperation and
+            collaboration; for managing and facilitating.
+          </li>
+        </ul>
+        <p>
           Now that you know your challenges and potential activities, you will
-          start to choose your methods. The methods are categorised around:
+          tailor a toolkit of methods for your situation. The methods are
+          categorised around:
         </p>
         <ul>
           <li>Finding</li>
@@ -18,6 +36,12 @@ const AboutMethods = () => {
           <li>Collaborating</li>
           <li>Managing</li>
         </ul>
+        <p>
+          Think carefully about how each method relates to your persona,
+          challenges and activities. You have a budget of 15 resources. Each
+          method has a cost represented in stars. Spend your budget wisely and
+          feel free to review your scenario at any time.
+        </p>
       </Content>
       <Footer
         backLink="/activities"
