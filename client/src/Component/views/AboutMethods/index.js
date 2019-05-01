a823cbfa854064ade2b142959a1e626c0fdf4dba
Kristina Jaggard <jaggardk@gmail.com>
2019-05-01 16:35:07 +0100

    about methods page

--- client/src/Component/views/AboutMethods/index.js
+++ client/src/Component/views/AboutMethods/index.js
@@ -9,15 +9,8 @@ const AboutMethods = () => {
       <Header headerImg={null} titleText="Choosing Methods" />
       <Content>
         <p>
-          As a community connector, you will be planning some activities to meet
-          the challenges you address. Some you can undertake yourself - some
-          might require development work by the hub team developing the mapping
-          and communication platform. Activities might include mapping local
-          people and organisation; making connections; helping develop projects;
-          sharing information and ideas; telling stories; managing more
-          effectively. We can provide you with some ideas about the methods that
-          could be useful to support these activities. The methods are
-          categorised around:
+          Now that you know your challenges and potential activities, you will
+          start to choose your methods. The methods are categorised around:
         </p>
         <ul>
           <li>Finding</li>
