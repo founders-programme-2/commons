928fdae106e62269258b6dfc04d1d0a5f2b1af76
ali <ali.7@outlook.com>
2019-05-05 00:28:44 +0300

    update-pdf-doc-component-after-moving-styles relate #160

--- client/src/Component/PDFDoc/index.js
+++ client/src/Component/PDFDoc/index.js
@@ -1,46 +1,7 @@
 import React, { Component } from 'react';
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document } from '@react-pdf/renderer';
 import methodCardData from '../../data/methodCardData';
-
-const styles = StyleSheet.create({
-  page: {
-    backgroundColor: '#E4E4E4',
-  },
-  section: {
-    margin: 5,
-    padding: 5,
-  },
-  commons: {
-    fontSize: 40,
-    marginTop: 30,
-    marginBottom: 1,
-    textTransform: 'uppercase',
-    textAlign: 'center',
-  },
-  title: {
-    fontSize: 30,
-    marginTop: 20,
-    marginBottom: 3,
-    textTransform: 'uppercase',
-    textAlign: 'center',
-  },
-  subTitle: {
-    fontSize: 20,
-    margin: 15,
-    marginLeft: 20,
-  },
-  name: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    margin: 5,
-    marginLeft: 20,
-    color: 'red',
-  },
-  span: {
-    fontWeight: 'bolder',
-    textDecoration: 'underline',
-  },
-});
+import styles from './index.style';
 
 class PdfSummary extends Component {
   state = {
